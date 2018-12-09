0058e43b3e5cfb7984a613bf4f326d1cddddce3f
Munim Dibosh <md-abdul_munim@astro.com.my>
2018-12-09 17:20:25 +0800

    Add a common error handler

---
+++ server/core/commonErrorHandler.js
@@ -0,0 +1,25 @@
+const statusCodes = require('http-status-codes');
+
+const logger = require('./logger');
+
+/**
+ * Takes care of any uncaught error that was not handled specifically for
+ * a specific app instance.
+ * @param app Express app instance
+ */
+function attachWithApp(app) {
+  app.use((err, req, res, next) => {
+    if (!err) {
+      next();
+    }
+
+    logger.log('Error: %s %O', err.message, err.stack);
+    res.status(statusCodes.INTERNAL_SERVER_ERROR).send({
+      'message': 'Internal Server Error'
+    });
+  });
+}
+
+module.exports = {
+  attachWithApp
+};
