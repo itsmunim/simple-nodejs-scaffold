955710e39de7f4e501234ca3df59d6fb20069983
Munim Dibosh <md-abdul_munim@astro.com.my>
2018-12-10 00:02:48 +0800

    Use the new changes & hook up the api endpoint

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const bootstrapper = require('./server/core/bootstrapper');
 const shutDownManager = require('./server/core/shutdownManager');
 const logger = require('./server/core/logger');
 const config = require('./server/config');
+const apiRoute = require('./server/api');
 
-let app = bootstrapper.initiate();
+function _bootstrap_() {
+  let frontEndDir = process.env.CLIENT_DIR || null;
+  let indexPath = process.env.INDEX || null;
+  let staticDirPath = process.env.STATIC_DIR || null;
+
+  if (frontEndDir) {
+    return bootstrapper.initiate(frontEndDir);
+  }
+
+  if (indexPath && staticDirPath) {
+    return bootstrapper.initiateWithIndexAndStaticDir(indexPath, staticDirPath);
+  }
+
+  return bootstrapper.initiate();
+}
+
 let port = process.env.PORT || config.DEFAULT_PORT;
+let app = _bootstrap_();
+
+app.use('/api', apiRoute);
 
 let server = app.listen(port, function () {
   logger.log(`Server started on port ${port}`);
