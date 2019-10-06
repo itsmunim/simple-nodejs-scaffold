18e7f580bc4ae9b88a50cc622d7e6fe212ace5f7
dibosh <md-abdul_munim@astro.com.my>
2019-10-06 16:22:16 +0800

    Create index for server & expose necessary methods

---
+++ server/index.js
@@ -0,0 +1,66 @@
+// start - initialize module aliases
+require('module-alias/register');
+// end - initialize module aliases
+
+const apiRoute = require('@api');
+const bootstrapper = require('@core/bootstrapper');
+const commonErrorHandler = require('@core/commonErrorHandler');
+const config = require('@config');
+const shutDownManager = require('@core/shutdownManager');
+const logger = require('@core/logger');
+
+/**
+ * Creates a server instance
+ * @param {*} options Options; can contain either a `clientPath` or `indexPath` together with
+ * a `resourcesPath`.
+ */
+function start(options) {
+  options = options || {};
+  const port = options.port || config.DEFAULT_PORT;
+  const app = _createApp(options);
+  return app.listen(port, function () {
+    logger.log(`server started on port ${port}`);
+  });
+}
+
+/**
+ * Manages force shutdown cases by closing all the open connections
+ * and house keeping.
+ * @param {*} server An server instance created by invoking `start`
+ */
+function autoManageShutdown(server) {
+  shutDownManager.manage(server);
+}
+
+/**
+ * Helper method to create an express app with necessary setup.
+ * @param {*} options Options; can contain either a `clientPath` or `indexPath` together with
+ * a `resourcesPath`
+ */
+function _createApp(options) {
+  let app;
+  options = options || {};
+  const { clientDirPath, indexPath, staticDirPath } = options;
+  /**
+   * App is bootstrapped only after all the models and routes have
+   * been loaded using `apiRoute`
+   */
+  if (indexPath && staticDirPath) {
+    app = bootstrapper.initiateWithIndexAndStaticDir(indexPath, staticDirPath);
+  } else {
+    app = bootstrapper.initiate(clientDirPath);
+  }
+
+  // apis are available under /api prefix
+  app.use('/api', apiRoute);
+
+  // error handler middleware has to be attached at the very end
+  commonErrorHandler.attachWithApp(app);
+
+  return app;
+}
+
+module.exports = {
+  autoManageShutdown,
+  start
+};
