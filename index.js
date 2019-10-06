7382d2b31cca926e12c2ec2b6e73ce641581e4f1
dibosh <md-abdul_munim@astro.com.my>
2019-10-06 16:22:42 +0800

    Use server exposed methods   - Bare minimum entry point

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
-// start - initialize module aliases
-require('module-alias/register');
-// end - initialize module aliases
 
-const bootstrapper = require('@core/bootstrapper');
-const shutDownManager = require('@core/shutdownManager');
-const logger = require('@core/logger');
-const config = require('@config');
-const apiRoute = require('@api');
+const server = require('./server');
 
-function _bootstrap_() {
-  let frontEndDir = process.env.CLIENT_DIR || null;
-  let indexPath = process.env.INDEX || null;
-  let staticDirPath = process.env.STATIC_DIR || null;
-
-  if (frontEndDir) {
-    return bootstrapper.initiate(frontEndDir);
-  }
-
-  if (indexPath && staticDirPath) {
-    return bootstrapper.initiateWithIndexAndStaticDir(indexPath, staticDirPath);
-  }
-
-  return bootstrapper.initiate();
+function options() {
+  let options = {};
+  options.port = process.env.PORT;
+  options.clientDirPath = process.env.CLIENT_DIR || null;
+  options.indexPath = process.env.INDEX || null;
+  options.staticDirPath = process.env.STATIC_DIR || null;
+  return options;
 }
 
-let port = process.env.PORT || config.DEFAULT_PORT;
-let app = _bootstrap_();
-
-app.use('/api', apiRoute);
-
-let server = app.listen(port, function () {
-  logger.log(`Server started on port ${port}`);
-});
-
-// Manage graceful shutdown and proper logging
-shutDownManager.manage(server);
+server.autoManageShutdown(server.start(options()));
